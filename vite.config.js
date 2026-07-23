705c877ff49597d8ed21ab8d07ff7fcdaa884799
Suraj Sawant <surajonenine@gmail.com>
2026-07-23 18:23:57 +0530

    fix(styles): set relative base in vite.config.js and enable @tailwindcss/vite plugin for offline 100% compiled CSS styling in Android WebView

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  base: './',
+  plugins: [
+    tailwindcss(),
+  ],
   build: {
     outDir: 'dist',
     rollupOptions: {
